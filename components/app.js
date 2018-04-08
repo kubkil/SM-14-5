3cc209a9da65b377ea056a2f3927483366a9e7e1
Jakub Kilar <kuba.kilar@gmail.com>
2018-04-08 19:17:17 +0200

    refactor

--- components/app.js
+++ components/app.js
@@ -29,7 +29,7 @@ const App = React.createClass({
   render: function () {
     return (
       React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: ContactForm}),
+        React.createElement(ContactForm, {contact: contactForm}),
         React.createElement(Contacts, {contact: contacts}, {})
       )
     );
